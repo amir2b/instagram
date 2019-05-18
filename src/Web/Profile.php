3cf4287e70e950e45e54172679c2bb85b9059f5f
Amir Bashiri <a.bashiri@lifeweb.ir>
2019-05-18 21:28:04 +0430

    add followers crawler

--- src/Web/Profile.php
+++ src/Web/Profile.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Storage;
 
 class Profile
 {
-    public static function following()
+    public static function following($end_cursor)
     {
         ## make cookie
         if (Storage::exists('cookie.json')) {
@@ -24,7 +24,7 @@ class Profile
         ]);
 
         ## fetch following
-        $response = $client->get('graphql/query/?query_hash=c56ee0ae1f89cdbd1c89e2bc6b8f3d18&variables=%7B%22id%22%3A%2212043191885%22%2C%22include_reel%22%3Atrue%2C%22fetch_mutual%22%3Afalse%2C%22first%22%3A24%7D', [
+        $response = $client->get('graphql/query/', [
             'cookies' => $jar,
             'headers' => [
                 'User-Agent' => config('instagram.user_agent'),
@@ -35,6 +35,16 @@ class Profile
                 'X-Instagram-Gis' => 'd6d2c3f877e447f598bf0bed0bfa88e4',
             ],
             'http_errors' => false,
+            'query' => [
+                'query_hash' => 'c56ee0ae1f89cdbd1c89e2bc6b8f3d18',
+                'variables' => json_encode([
+                    'id' => '12043191885',
+                    'include_reel' => 'true',
+                    'fetch_mutual' => 'false',
+                    'first' => 24,
+                    'after' => $end_cursor,
+                ]),
+            ],
         ]);
 
         ## save cookie
@@ -43,8 +53,62 @@ class Profile
         ## parse json
         $response_json = json_decode($response->getBody(), true);
 
-        if($response_json['status'] === 'ok') {
+        if ($response_json['status'] === 'ok') {
             return $response_json['data']['user']['edge_follow'];
+        }
+
+        dd($response_json);
+
+        return [];
+    }
+
+    public static function followers($end_cursor)
+    {
+        ## make cookie
+        if (Storage::exists('cookie.json')) {
+            $cookies = json_decode(Storage::get('cookie.json'), true);
+            $jar = new CookieJar(false, $cookies);
+        } else {
+            $jar = new CookieJar();
+        }
+
+        ## make client
+        $client = new Client([
+            'base_uri' => 'https://www.instagram.com/',
+        ]);
+
+        ## fetch following
+        $response = $client->get('graphql/query/', [
+            'cookies' => $jar,
+            'headers' => [
+                'User-Agent' => config('instagram.user_agent'),
+                'Origin' => 'https://www.instagram.com',
+                'Referer' => 'https://www.instagram.com/amir.2b/following/',
+                'X-Requested-With' => 'XMLHttpRequest',
+                'x-ig-app-id' => '936619743392459',
+                'X-Instagram-Gis' => 'd6d2c3f877e447f598bf0bed0bfa88e4',
+            ],
+            'http_errors' => false,
+            'query' => [
+                'query_hash' => '56066f031e6239f35a904ac20c9f37d9',
+                'variables' => json_encode([
+                    'id' => '12043191885',
+                    'include_reel' => 'true',
+                    'fetch_mutual' => 'false',
+                    'first' => 24,
+                    'after' => $end_cursor,
+                ]),
+            ],
+        ]);
+
+        ## save cookie
+        Storage::put('cookie.json', json_encode($jar->toArray()));
+
+        ## parse json
+        $response_json = json_decode($response->getBody(), true);
+
+        if ($response_json['status'] === 'ok') {
+            return $response_json['data']['user']['edge_followed_by'];
         }
 
         dd($response_json);
